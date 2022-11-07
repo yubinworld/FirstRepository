2631701420603ba6df31b901df0b094a9c225a32
yubinworld <myoobin.i@gmail.com>
2022-11-07 12:09:29 +0900

    첫번째커밋

---
+++ src/git/Hellogit.java
@@ -0,0 +1,18 @@
+package git;
+
+public class Hellogit {
+	//git 만드는 법
+	
+	/*
+	 시작메뉴 -> Git -> Git Bash 를 실행하여 사용자 이름과 이메일을 설정한다.
+	$ <- 제외하기 git config --global user.name "사용자 이름"
+				git config --global user.email "이메일" 
+	 */
+	
+
+	public static void main(String[] args) {
+
+		System.out.println("git 연동해보기");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author yubin
+ *
+ */
+module Git {
+}
